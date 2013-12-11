88b8a790680ea69161975b4d3b91a4be5cbd53e3
Julian Lewis <julian.lewis@cern.ch>
2013-12-11 16:26:27 +0100

    ctrp/v/test: Starting work here
    Signed-off-by: Julian Lewis <julian.lewis@cern.ch>

---
+++ ctr-2.0/driver/test/CtrOpen.c
@@ -0,0 +1,20 @@
+/**************************************************************************/
+/* Open the Ctr driver                                                    */
+/**************************************************************************/
+
+#include <ctrdrvr.h>
+
+static CtrDrvrDevice ctr_device = CtrDrvrDeviceANY;
+static char *devnames[CtrDrvrDEVICES] = {"ctr", "ctri", "ctrp", "ctrv", "ctre" };
+
+/* ======================== */
+
+int CtrOpen() {
+
+char fnm[32];
+int fn;
+
+   sprintf(fnm,"/dev/%s.1",devnames[ctr_device]);
+   if ((fn = open(fnm,O_RDWR,0)) > 0) return(fn);
+   return(0);
+}
