2d73bb33f411d3244a02aee453f27ce741faa5ad
Juan David Gonzalez Cobas <dcobas@cern.ch>
2014-01-14 10:48:40 +0100

    ipack: do not use perl scripts anymore
    They are not needed to install carriers/mezzanines,
    as all the topological information can be inferred from
    the sysfs data, crateconfig and transfer.ref

--- ipack/scripts/tpci200install.sh
+++ ipack/scripts/tpci200install.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-./tpci200_install.pl $*
+(cat /proc/modules | grep -q "^ipack ") || /sbin/insmod ../ipack/ipack.ko
+(cat /proc/modules | grep -q "^tpci200 ") || /sbin/insmod ../ipack/tpci200.ko
+
+./make_dev_nodes
