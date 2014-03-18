5d3557fa10165925ef34dd2a8923a2ec2e4c9568
Julian Lewis <julian.lewis@cern.ch>
2014-03-18 16:09:59 +0100

    Use old open algorithum in loop in case calling old driver from new test program
    Signed-off-by: Julian Lewis <julian.lewis@cern.ch>

--- ctr-2.0/driver/test/CtrOpen.c
+++ ctr-2.0/driver/test/CtrOpen.c
@@ -3,18 +3,29 @@
 /**************************************************************************/
 
 #include <ctrdrvr.h>
+#include <errno.h>
 
 static CtrDrvrDevice ctr_device = CtrDrvrDeviceANY;
 static char *devnames[CtrDrvrDEVICES] = {"ctr", "ctri", "ctrp", "ctrv", "ctre" };
-
 /* ======================== */
 
 int CtrOpen() {
 
-char fnm[32];
-int fn;
+	char fnm[32];
+	int fn, i;
 
-   sprintf(fnm,"/dev/%s.1",devnames[ctr_device]);
-   if ((fn = open(fnm,O_RDWR,0)) > 0) return(fn);
-   return(0);
+	/* 
+	 * this loop ensures backward compatibility with the old driver
+	 * which assign a single client per ctr.i device
+	 * if the old driver is installed, this loop tries to find
+	 * the first ctr.i device which is not BUSY
+	 * if the new driver is installed the open succeed always with
+	 * ctr.1 device.
+	 */
+	for(i=1; i<=CtrDrvrCLIENT_CONTEXTS; ++i) {
+		sprintf(fnm,"/dev/%s.%1d",devnames[ctr_device], i);
+   		if ((fn = open(fnm,O_RDWR,0)) > 0)
+			break;
+	}
+	return(fn);
 }
