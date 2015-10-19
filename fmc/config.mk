8291b388c559076734292b8e1f6bb7b6e1535981
Adam Wujek <adam.wujek@cern.ch>
2015-10-19 17:34:37 +0200

    fmc: import fmc submodules
    --zio
    --fmc-bus
    --spec-sw
    --svec-sw
    --fine-delay-sw
    --fmc-adc-100m14b4cha-sw
    --fmc-tdc-sw
    --obsbox
    
    Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

---
+++ fmc/config.mk
@@ -0,0 +1,13 @@
+#
+# Use CONFIG_ macros, even if we are not yet using Kconfig. Use "m", not "y".
+# Most of these can be set to =n in the environment to disable building
+# the respective module
+
+CONFIG_FMC = m
+CONFIG_ZIO = m
+CONFIG_SPEC ?= m
+CONFIG_SVEC ?= m
+CONFIG_FDELAY ?= m
+CONFIG_FMC_ADC ?= m
+CONFIG_OBSBOX ?= m
+CONFIG_FMC_TDC ?= m
---
+++ fmc/parent_common.mk
@@ -0,0 +1,36 @@
+CPU?=L866
+REPO_PARENT ?= $(shell pwd)
+include $(REPO_PARENT)/../common.mk
+include /acc/dsc/src/co/Make.auto
+
+#overwrite rules from Make.auto
+%: %.o
+	$(LINK.o) $^  $(LOADLIBES) $(LDLIBS) -o $@
+
+%.o: %.c
+	$(COMPILE.c) $< $(OUTPUT_OPTION)
+
+%: %.c
+	$(LINK.c) $^  $(LOADLIBES) $(LDLIBS) -o $@
+
+LINUX=$(KERNELSRC)
+CROSS_COMPILE=$(TOOLS)
+export LINUX
+export CC
+export LD
+export CROSS_COMPILE
+
+
+FMC_BUS ?= $(REPO_PARENT)/fmc-bus
+export FMC_BUS
+ZIO ?= $(REPO_PARENT)/zio
+export ZIO
+SPEC_SW ?= $(REPO_PARENT)/spec-sw
+export SPEC_SW
+SVEC_SW ?= $(REPO_PARENT)/svec-sw
+export SVEC_SW
+
+CONFIG_SUPER_REPO ?= coht
+CONFIG_SUPER_REPO_VERSION ?= $(shell cd $(REPO_PARENT); git describe --always --dirty --long --tags)
+export CONFIG_SUPER_REPO
+export CONFIG_SUPER_REPO_VERSION
