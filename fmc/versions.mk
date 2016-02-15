bbbe91fb0409dbe8f493be4c6b62c3b90c1ba676
Adam Wujek <adam.wujek@cern.ch>
2016-02-15 11:33:18 +0100

    fmc: move version information to versions.mk
    Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

---
+++ fmc/versions.mk
@@ -0,0 +1,24 @@
+# common versions
+VER_MAJOR=1
+VER_MINOR=0
+VER_PATCH=0
+
+# version of the programs
+PROG_MAJOR=$(VER_MAJOR)
+PROG_MINOR=$(VER_MINOR)
+PROG_PATCH=$(VER_PATCH)
+
+# version of the header file(s)
+HEADER_MAJOR=$(VER_MAJOR)
+HEADER_MINOR=$(VER_MINOR)
+HEADER_PATCH=$(VER_PATCH)
+
+# version of the library
+LIB_MAJOR=$(VER_MAJOR)
+LIB_MINOR=$(VER_MINOR)
+LIB_PATCH=$(VER_PATCH)
+
+# define versions
+VER_PREV=
+VER_CURRENT=$(VER_MAJOR).$(VER_MINOR)
+VER_NEXT=
