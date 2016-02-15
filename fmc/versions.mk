4370a1a09c75101ea2042bf1d06e97394ece6ca3
Adam Wujek <adam.wujek@cern.ch>
2016-02-15 11:54:54 +0100

    fmc: use separate version information for each fmc
    Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

--- fmc/versions.mk
+++ fmc/versions.mk
@@ -1,24 +1,76 @@
-# common versions
-VER_MAJOR=1
-VER_MINOR=0
-VER_PATCH=0
-
-# version of the programs
-PROG_MAJOR=$(VER_MAJOR)
-PROG_MINOR=$(VER_MINOR)
-PROG_PATCH=$(VER_PATCH)
-
-# version of the header file(s)
-HEADER_MAJOR=$(VER_MAJOR)
-HEADER_MINOR=$(VER_MINOR)
-HEADER_PATCH=$(VER_PATCH)
-
-# version of the library
-LIB_MAJOR=$(VER_MAJOR)
-LIB_MINOR=$(VER_MINOR)
-LIB_PATCH=$(VER_PATCH)
+###########################################
+# define version of the zio
+zio_MAJOR=1
+zio_MINOR=0
+zio_PATCH=0
 
 # define versions
-VER_PREV=
-VER_CURRENT=$(VER_MAJOR).$(VER_MINOR)
-VER_NEXT=
+zio_PREV=
+zio_CURRENT=$(zio_MAJOR).$(zio_MINOR)
+zio_NEXT=
+
+###########################################
+# define version of the spec-sw
+spec-sw_MAJOR=1
+spec-sw_MINOR=0
+spec-sw_PATCH=0
+
+# define versions
+spec-sw_PREV=
+spec-sw_CURRENT=$(spec-sw_MAJOR).$(spec-sw_MINOR)
+spec-sw_NEXT=
+
+###########################################
+# define version of the svec-sw
+svec-sw_MAJOR=1
+svec-sw_MINOR=0
+svec-sw_PATCH=0
+
+# define versions
+svec-sw_PREV=
+svec-sw_CURRENT=$(svec-sw_MAJOR).$(svec-sw_MINOR)
+svec-sw_NEXT=
+
+###########################################
+# define version of the fine-delay
+fine-delay-sw_MAJOR=2
+fine-delay-sw_MINOR=0
+fine-delay-sw_PATCH=4
+
+# define versions
+fine-delay-sw_PREV=1.0
+fine-delay-sw_CURRENT=$(fine-delay-sw_MAJOR).$(fine-delay-sw_MINOR)
+fine-delay-sw_NEXT=
+
+###########################################
+# define version of the obsbox
+obsbox_MAJOR=1
+obsbox_MINOR=0
+obsbox_PATCH=0
+
+# define versions
+obsbox_PREV=
+obsbox_CURRENT=$(obsbox_MAJOR).$(obsbox_MINOR)
+obsbox_NEXT=
+
+###########################################
+# define version of the fmc-tdc-sw
+fmc-tdc-sw_MAJOR=1
+fmc-tdc-sw_MINOR=0
+fmc-tdc-sw_PATCH=0
+
+# define versions
+fmc-tdc-sw_PREV=
+fmc-tdc-sw_CURRENT=$(fmc-tdc-sw_MAJOR).$(fmc-tdc-sw_MINOR)
+fmc-tdc-sw_NEXT=
+
+###########################################
+# define version of the fmc-adc
+fmc-adc_MAJOR=1
+fmc-adc_MINOR=0
+fmc-adc_PATCH=0
+
+# define versions
+fmc-adc_PREV=
+fmc-adc_CURRENT=$(fmc-adc_MAJOR).$(fmc-adc_MINOR)
+fmc-adc_NEXT=
